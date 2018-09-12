d4bff6ccedf54ebc15d8dfea1523db6759371b9b
Jan Dietrich <dietrich@pik-potsdam.de>
2018-09-12 22:15:28 +0200

    added functions read_cff and cff2bibentry

---
+++ R/cff2bibentry.R
@@ -0,0 +1,30 @@
+#' cff2bibentry
+#' 
+#' Converter citation file format to bibentry
+#' 
+#' @param x path to citation file or read in citation in citation file format
+#' @author Jan Philipp Dietrich
+#' @importFrom utils person as.personList bibentry
+#' @export
+
+cff2bibentry <- function(x) {
+  if(is.character(x) && file.exists(x)) x <- read_cff(x)
+  #convert authors
+  authors <- list()
+  for(i in 1:length(x$authors)) {
+    authors[[i]] <- person(given=x$authors[[i]][["given-names"]],
+                             family=x$authors[[i]][["family-names"]],
+                             email=x$authors[[i]][["email"]])
+  }
+  x$authors <- as.personList(authors)
+  if(!is.null(x$version)) x$title <-paste(x$title,"- Version",x$version)
+  
+  bibentry(bibtype="Misc",
+           title = x$title,
+           author = x$authors,
+           doi = x$doi,
+           date = x$`date-released`,
+           year = format(as.Date(x$`date-released`),"%Y"),
+           url=x$url)
+  
+}
---
+++ R/read_cff.R
@@ -0,0 +1,14 @@
+#' read_cff
+#' 
+#' Read a citation file format  file (https://citation-file-format.github.io/)
+#' 
+#' @param file path to the citation file
+#' @author Jan Philipp Dietrich
+#' @importFrom yaml read_yaml
+#' @export
+
+read_cff <- function(file) {
+  cff <- read_yaml(file)
+  if(is.numeric(cff$version)) cff$version <- format(cff$version,nsmall=1)
+  return(cff)
+}
