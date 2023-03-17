21a8c704c0cb4f0e593228fb14ec0e4bdeadc994
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-03-17 13:41:04 +0100

    added scanRepos (experimental!)

---
+++ R/scanRepos.R
@@ -0,0 +1,35 @@
+#' @title scan repositories
+#' @description Scan repositories for existing CITATION.cff information
+#' @param repos character vector of repository urls to be scanned
+#' @return A list with CITATION.cff information collected from the
+#' provided repositories
+#' @author Jan Philipp Dietrich
+#' @export
+#' @examples
+#' scanRepos(c("https://github.com/magpiemodel/magpie",
+#'             "https://github.com/remindmodel/remind"))
+#' @seealso read_cff
+
+scanRepos <- function(repos) {
+  guessCITATIONurl <- function(url) {
+    if (basename(url) == "CITATION.cff") return(url)
+    if (grepl("github.com", url, fixed = TRUE)) {
+      url <- sub("github.com", "raw.githubusercontent.com",
+                 file.path(url, "HEAD/CITATION.cff"), fixed = TRUE)
+      return(url)
+    }
+    return(file.path(url, "CITATION.cff"))
+  }
+  cff <- list()
+  for (r in repos) {
+     url <- guessCITATIONurl(r)
+     tryCatch(
+{
+       cff[[r]] <- suppressWarnings(read_cff(url))
+       message("read in CITATION.cff for ", r)
+       },
+ error = function(e) warning("CITATION.cff for ", r, " not found", call. = FALSE))
+     if (class(cff$r) %in% "try-error") cff$r <- "MISSING"
+  }
+  return(cff)
+}
